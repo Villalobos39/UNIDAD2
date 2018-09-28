87f06908e6ea42e09a0ed918f2b40919f6f8b274
DULCE VILLALOBOS <42878496+Villalobos39@users.noreply.github.com>
2018-09-27 22:40:49 -0700

    Initial commit

---
+++ T01_U2_VILLALOBOS_PEREZ_DULCE_JSMIN/RECURSIVIDAD_FACTORIA/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace RECURSIVIDAD_FACTORIA
+{
+    class Program
+    {
+        public static int Factorial(int numero) // LA LLAMADA DEL METODO RECURSIVAMENTE 
+        {
+            if (numero==0) // si n es igual a 0 , la condicion establece que regrese n = 1 
+            {
+                return 1;
+            }
+            return numero * Factorial(numero - 1);
+        }
+        static void Main(string[] args)
+        {
+            Stopwatch timeEjecucion = Stopwatch.StartNew();
+            int numero; // factoria numero 0 = 1
+            Console.Write("\n\t *** FACTORIAL DE UN NUMERO POR METODOS *** ");
+            Console.Write("\n\t INGRESE NUMERO  :");
+            numero = int.Parse(Console.ReadLine());         
+             Console.Write("\n\t EL RESULTADO FACTORIA DE {0} ES : {1} \n\t TIEMPO DE EJECUCION {2}  ", numero, Factorial(numero) , timeEjecucion.Elapsed);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ T01_U2_VILLALOBOS_PEREZ_DULCE_JSMIN/n_FACTORIAL/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace n_FACTORIAL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch timeEjecucion = Stopwatch.StartNew();
+            int numero, factorial = 1; // factoria numero 0 = 1
+            Console.Write("\n\t *** FACTORIAL DE UN NUMERO POR UN CICLO *** "); 
+            Console.Write("\n\t INGRESE NUMERO  :"); 
+            numero = int.Parse(Console.ReadLine());
+            for (int contar = 1; contar <= numero; contar++)
+            { // factorial = 1*1=1 factorial =1*2=2 factorial =2*3=6 factorial=6*4=24 factorial=24*5=120 .....
+                factorial = factorial * contar;
+            }
+            Console.Write("\n\t EL RESULTADO FACTORIA DE {0} ES : {1} \n\t TIEMPO DE EJECUCION {2}  ", numero, factorial, timeEjecucion.Elapsed);
+            Console.ReadKey();
+        }
+    }
+}
