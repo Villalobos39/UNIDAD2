3037de7c8b8a9218a8574449f914dae367def17f
DULCE VILLALOBOS <42878496+Villalobos39@users.noreply.github.com>
2018-09-28 06:20:06 -0700

    practica

---
+++ U2_T1_VILALLALOBOS_PEREZ_DULCE/CLASES_RECURSIVIDAD/FACTORIAL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLASES_RECURSIVIDAD
+{
+    class FACTORIAL
+    {
+        public FACTORIAL() { }
+      //  FACTORIAL() { }
+
+        public int Factorial(int numero) // LA LLAMADA DEL METODO RECURSIVAMENTE 
+        {
+            if (numero == 0) // si n es igual a 0 , la condicion establece que regrese n = 1 
+            {
+                return 1;
+            }
+            return numero * Factorial(numero - 1);
+        }
+    }
+}
---
+++ U2_T1_VILALLALOBOS_PEREZ_DULCE/CLASES_RECURSIVIDAD/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+
+namespace CLASES_RECURSIVIDAD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch timeEjecucion = Stopwatch.StartNew();
+            
+            int numero; // factoria numero 0 = 1
+            Console.Write("\n\t *** FACTORIAL DE UN NUMERO POR METODOS *** ");
+            Console.Write("\n\t INGRESE NUMERO  :");
+            numero = int.Parse(Console.ReadLine());
+
+            FACTORIAL facror = new FACTORIAL();// objeto de la clases
+            Console.Write("\n\t EL RESULTADO FACTORIA DE {0} ES : {1} \n\t TIEMPO DE EJECUCION {2}  ", numero,facror.Factorial(numero), timeEjecucion.Elapsed);
+            Console.ReadKey();
+        }
+    }
+}
